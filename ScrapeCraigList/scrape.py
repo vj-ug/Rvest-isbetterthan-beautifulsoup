d73c18ac3db0a43f817a1cc1e2e8d9dd78d543e2
V.Jindal <vj-ug@users.noreply.github.com>
2015-10-06 12:44:26 -0500

    private to public

---
+++ ScrapeCraigList/scrape.py
@@ -0,0 +1,41 @@
+# This is a modification to another scraping script on github @Greg  
+#  the R Script  has been taken down becase Craiglist does not allow scraping
+
+def parse_results(search_term):
+    results = []
+    search_term = search_term.strip().replace(' ', '+')
+    search_url = BASE_URL.format(search_term)
+    soup = BeautifulSoup(urlopen(search_url).read())
+    rows = soup.find('div', 'content').find_all('p', 'row')
+    for row in rows:
+        url = 'http://chicago.craigslist.org' + row.a['href']
+        create_date = row.find('span', 'date').get_text()
+        title = row.find_all('a')[1].get_text()
+        results.append({'url': url, 'create_date': create_date, 'title': title})
+    return results
+    
+  def has_new_records(results):
+    current_posts = [x['url'] for x in results]
+    fields = results[0].keys()
+    if not os.path.exists('results.csv'):
+        return True
+
+    with open('results.csv', 'r') as f:
+        reader = csv.DictReader(f, fieldnames=fields, delimiter='|')
+        seen_posts = [row['url'] for row in reader]
+
+    is_new = False
+    for post in current_posts:
+        if post in seen_posts:
+            pass
+        else:
+            is_new = True
+    return is_new
+    
+    def write_results(results):
+    """Writes list of dictionaries to file."""
+    fields = results[0].keys()
+    with open('results.csv', 'w') as f:
+        dw = csv.DictWriter(f, fieldnames=fields, delimiter='|')
+        dw.writer.writerow(dw.fieldnames)
+        dw.writerows(results)
